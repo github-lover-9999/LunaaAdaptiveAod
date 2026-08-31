ad7e1ff1f2c987c3fd24fb3d0a8b1b8d8e89b118
github-lover-9999 <bol29@users.noreply.github.com>
2026-08-31 21:58:47 +0200

    release: v1.6.5 strictly numeric versioning with clean AOD brightness card

--- app/src/main/java/dev/lunaa/aod/AodReleaseInfo.java
+++ app/src/main/java/dev/lunaa/aod/AodReleaseInfo.java
@@ -1,9 +1,9 @@
 package dev.lunaa.aod;
 
 public final class AodReleaseInfo {
-    public static final String VERSION_NAME = "1.6.4";
-    public static final int VERSION_CODE = 16483;
-    public static final String VERSION_TAG = "v1.6.4s";
+    public static final String VERSION_NAME = "1.6.5";
+    public static final int VERSION_CODE = 16500;
+    public static final String VERSION_TAG = "v1.6.5";
 
     private AodReleaseInfo() {}
 }
